c36d8ba635e353cee402b937927642fb1f308dc7
upinetree <t.matsumoto@everyleaf.com>
2014-06-24 23:55:36 +0900

    Add simple CRUD for expenses

---
+++ app/controllers/expenses_controller.rb
@@ -0,0 +1,49 @@
+class ExpensesController < ApplicationController
+  before_action :set_expense, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @expenses = Expense.all
+  end
+
+  def show
+  end
+
+  def new
+    @expense = Expense.new
+  end
+
+  def edit
+  end
+
+  def create
+    @expense = Expense.new(expense_params)
+
+    if @expense.save
+      redirect_to @expense, notice: 'Expense was successfully created.'
+    else
+      render :new
+    end
+  end
+
+  def update
+    if @expense.update(expense_params)
+      redirect_to @expense, notice: 'Expense was successfully updated.'
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @expense.destroy
+    redirect_to expenses_url, notice: 'Expense was successfully destroyed.'
+  end
+
+  private
+  def set_expense
+    @expense = Expense.find(params[:id])
+  end
+
+  def expense_params
+    params.require(:expense).permit(:amount)
+  end
+end
