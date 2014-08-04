7eaff6965e39f2775b30023447dd736dcf4034d4
upinetree <t.matsumoto@everyleaf.com>
2014-08-04 23:19:10 +0900

    Add configs to spec_helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -17,6 +17,12 @@ Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
 ActiveRecord::Migration.maintain_test_schema!
 
 RSpec.configure do |config|
+  config.include FactoryGirl::Syntax::Methods
+
+  config.filter_run focus: true
+  config.filter_run_excluding skip: true
+  config.run_all_when_everything_filtered = true
+
   # ## Mock Framework
   #
   # If you prefer to use mocha, flexmock or RR, uncomment the appropriate line:
