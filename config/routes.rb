7e21dbeb7cae09db87a3349b4a21b4e47e18fa30
upinetree <t.matsumoto@everyleaf.com>
2014-08-17 01:07:22 +0900

    Add the navbar that has signout link

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   get 'sessions/new' => 'sessions#new', as: :signin
   match '/auth/:provider/callback' => 'sessions#create', via: [:get, :post]
   match '/auth/failure' => 'sessions#failure', via: :get
-  match '/signout' => 'sessions#destroy', via: :get
+  match '/signout' => 'sessions#destroy', via: :delete
 
   resources :users
 
