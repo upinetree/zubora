42e8c4dc0b252a40406469098cfa20d8527f54ae
upinetree <t.matsumoto@everyleaf.com>
2014-08-16 15:44:49 +0900

    Add validations to users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,10 @@
 class User < OmniAuth::Identity::Models::ActiveRecord
+  validates :email,           presence: true, uniqueness: true
+  validates :password_digest, presence: true
+  validates :screen_name,     presence:      true,
+                              uniqueness:    true,
+                              format: { with: /\A[a-z0-9_]+\z/i, message: "は半角英数'_'を使用してください。"},
+                              length: { in: 3..20 }
+
   auth_key 'screen_name'
 end
