2d47dc51e63bf9f92bc18429166797462cedefe1
upinetree <t.matsumoto@everyleaf.com>
2014-08-17 00:38:33 +0900

    Root to expenses index

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  root to: 'expenses#index'
+
   get 'sessions/new' => 'sessions#new', as: :signin
   match '/auth/:provider/callback' => 'sessions#create', via: [:get, :post]
   match '/auth/failure' => 'sessions#failure', via: :get
