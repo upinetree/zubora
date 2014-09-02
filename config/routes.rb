69916605829ec11f7ca970cdf44c08a8eda2ccb9
upinetree <t.matsumoto@everyleaf.com>
2014-09-02 22:08:04 +0900

    Route users#new to signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,14 @@
 Rails.application.routes.draw do
   root to: 'expenses#index'
 
-  get 'sessions/new' => 'sessions#new', as: :signin
+  get 'signin' => 'sessions#new', as: :signin
   match '/auth/:provider/callback' => 'sessions#create', via: [:get, :post]
   match '/auth/failure' => 'sessions#failure', via: :get
   match '/signout' => 'sessions#destroy', via: :delete
 
-  resources :users, except: [:edit, :destroy]
-  get '/profile' => 'users#edit', as: :profile, via: :get
+  resources :users, except: [:new, :edit, :destroy]
+  get '/signup'  => 'users#new', as: :signup
+  get '/profile' => 'users#edit', as: :profile
 
   resources :expenses, except: [:new, :edit]
 end
