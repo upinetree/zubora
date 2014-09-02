4694ab6ab7a970cd911ee40704d35138c2c5c6bc
upinetree <t.matsumoto@everyleaf.com>
2014-09-02 22:17:20 +0900

    Remove unneeded options `:as` in routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,14 @@
 Rails.application.routes.draw do
   root to: 'expenses#index'
 
-  get    '/signin'  => 'sessions#new', as: :signin
+  get    '/signin'  => 'sessions#new'
   delete '/signout' => 'sessions#destroy'
   match  '/auth/:provider/callback' => 'sessions#create',  via: [:get, :post]
   get    '/auth/failure'            => 'sessions#failure'
 
   resources :users, except: [:new, :edit, :destroy]
-  get '/signup'  => 'users#new', as: :signup
-  get '/profile' => 'users#edit', as: :profile
+  get '/signup'  => 'users#new'
+  get '/profile' => 'users#edit'
 
   resources :expenses, except: [:new, :edit]
 end
