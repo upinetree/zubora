87009d05741871c40b94d5156d37750bb7a07948
upinetree <t.matsumoto@everyleaf.com>
2014-09-02 22:15:35 +0900

    Simplify routes for sessions

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
   root to: 'expenses#index'
 
-  get 'signin' => 'sessions#new', as: :signin
-  match '/auth/:provider/callback' => 'sessions#create', via: [:get, :post]
-  match '/auth/failure' => 'sessions#failure', via: :get
-  match '/signout' => 'sessions#destroy', via: :delete
+  get    '/signin'  => 'sessions#new', as: :signin
+  delete '/signout' => 'sessions#destroy'
+  match  '/auth/:provider/callback' => 'sessions#create',  via: [:get, :post]
+  get    '/auth/failure'            => 'sessions#failure'
 
   resources :users, except: [:new, :edit, :destroy]
   get '/signup'  => 'users#new', as: :signup
