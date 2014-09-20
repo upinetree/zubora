c19cf99ea5a2f59427f6cbd8fbf1187e53736654
upinetree <t.matsumoto@everyleaf.com>
2014-09-20 15:22:05 +0900

    Save expense with a default value when accrued_on is nil

--- app/models/expense.rb
+++ app/models/expense.rb
@@ -2,4 +2,12 @@ class Expense < ActiveRecord::Base
   belongs_to :user
 
   validates :amount, presence: true
+
+  before_validation :set_defaults
+
+  private
+
+  def set_defaults
+    self.accrued_on ||= Date.today
+  end
 end
--- spec/models/expense_spec.rb
+++ spec/models/expense_spec.rb
@@ -8,4 +8,12 @@ describe Expense do
       expect(expense).not_to be_valid
     end
   end
+
+  context 'default values' do
+    let(:expense) { build(:expense, accrued_on: nil) }
+    it 'accrued_onが空のとき、今日の日付が保存される' do
+      expense.save
+      expect(expense.accrued_on).to eq(Date.today)
+    end
+  end
 end
